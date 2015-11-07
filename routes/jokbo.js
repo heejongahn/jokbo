bd3c520794c1af1bb8af0799bf4bacea7d68f3de
Heejong Ahn <heejongahn@gmail.com>
2015-11-07 23:49:09 +0900

    More pretty dummy

--- routes/jokbo.js
+++ routes/jokbo.js
@@ -4,9 +4,9 @@ var router = express.Router();
 var dummy_data =
   [{
       "year": 2015,
-        "sememster": "spring",
-          "professor": "최광무",
-            "subject": "형식언어및오토마타",
+      "semester": "spring",
+      "professor": "최광무",
+      "subject": "형식언어및오토마타",
   }];
 
 /* GET home page. */
